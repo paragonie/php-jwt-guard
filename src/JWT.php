34414e1e1918dc6c28a8840d6dcf43b716818617
Paragon Initiative Enterprises <security@paragonie.com>
2021-08-11 17:25:46 -0400

    Address issue #1
    - We guard against invalid algorithms by partitioning by algorithm first
    - Fix the JWT::encode() wrapping
    
    Thanks @cschomburg for this report

--- src/JWT.php
+++ src/JWT.php
@@ -9,18 +9,28 @@ use UnexpectedValueException;
 class JWT extends FirebaseJwt
 {
     /**
+     * @link FirebaseJwt::encode()
+     *
      * @param JWTKey|array|object $payload
      * @param resource|string $key
      * @param string $alg
      * @param null $keyId
      * @param null $head
      * @return string
+     *
+     * @throws SodiumException
      */
     public static function encode($payload, $key, $alg = 'HS256', $keyId = null, $head = null)
     {
         if ($key instanceof KeyRing) {
-            if ($keyId) {
+            if (!$keyId) {
                 throw new InvalidArgumentException('No key ID specified for a keyring');
+            }
+
+            // Partition the set of keys by the algorithm selected.
+            $key = $key->partition($alg);
+            if (empty($key[$keyId])) {
+                throw new InvalidArgumentException('Key not found (or wrong algorithm)');
             }
             $key = $key[$keyId];
         }
@@ -31,6 +41,8 @@ class JWT extends FirebaseJwt
     }
 
     /**
+     * @link FirebaseJwt::decode()
+     *
      * @param string $jwt
      * @param KeyRing|JWTKey|array|resource|string $key
      * @param array $allowed_algs
@@ -53,21 +65,18 @@ class JWT extends FirebaseJwt
                 throw new UnexpectedValueException('Invalid header encoding');
             }
 
+            // Partition the set of keys by the algorithm selected.
+            $key = $key->partition($header->alg);
+
             if (isset($header->kid)) {
                 if (!isset($key[$header->kid])) {
-                    throw new UnexpectedValueException('"kid" invalid, unable to lookup correct key');
+                    throw new UnexpectedValueException(
+                        '"kid" invalid, unable to lookup correct key for given algorithm'
+                    );
                 }
                 $key = $key[$header->kid];
             } else {
                 throw new UnexpectedValueException('"kid" empty, unable to lookup correct key');
-            }
-
-            $alg_ok = false;
-            foreach ($allowed_algs as $alg) {
-                $alg_ok |= $key->isValidForAlg($alg);
-            }
-            if (!$alg_ok) {
-                throw new UnexpectedValueException('Incorrect algorithm.');
             }
 
             return parent::decode($jwt, $key->getKeyMaterial(), $allowed_algs);
--- tests/JWTTest.php
+++ tests/JWTTest.php
@@ -68,4 +68,39 @@ class JWTTest extends TestCase
         }
         $this->assertFalse($fail, 'Expected an exception; was disappointed');
     }
+
+    public function testConfusion()
+    {
+        $hsKey = hash('sha256', 'phpunit-test-key-for-issue-351');
+
+        $esPubkey = "-----BEGIN PUBLIC KEY-----\n" .
+            "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEVs/o5+uQbTjL3chynL4wXgUg2R9\n" .
+            "q9UU8I5mEovUf86QZ7kOBIjJwqnzD1omageEHWwHdBO6B+dFabmdT9POxg==\n" .
+            "-----END PUBLIC KEY-----";
+
+        $keyring = new KeyRing();
+        $keyring->with('HS256', 'foo', $hsKey);
+        $keyring->with('ES256', 'bar', $esPubkey);
+
+        $payload = array('sub' => 'phpunit');
+        $token = JWT::encode($payload, $esPubkey, 'HS256', 'bar'); // wrong algo
+
+        $fail = false;
+        try {
+            JWT::decode($token, $keyring, array('HS256', 'ES256'));
+            $fail = true;
+        } catch (UnexpectedValueException $ex) {
+        }
+        $this->assertFalse($fail, 'Expected an exception');
+    }
+
+    public function testEncode()
+    {
+        $keyring = new KeyRing();
+        $keyring->with('HS256', 'foo', hash('sha256', 'phpunit-test-key-for-issue-351'));
+
+        $payload = array('sub' => 'phpunit');
+        $encoded = JWT::encode($payload, $keyring, 'HS256', 'foo'); // should not throw exception
+        $this->assertIsString($encoded);
+    }
 }
